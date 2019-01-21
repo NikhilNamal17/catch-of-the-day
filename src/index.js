34f7da4f4aa3799c46f9ab8b79cbf7db3dc6330d
Nikhil N Namal <nikhilnamal16@gmail.com>
2019-01-21 21:35:47 +0530

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/Storepicker';
-import App from './components/App';
 import Router from './components/Router';
 import './css/style.css';
 
