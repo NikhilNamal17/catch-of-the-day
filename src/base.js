86708c1001bc6a7274e4b26b375ca22045d36b91
Nikhil N Namal <nikhilnamal16@gmail.com>
2019-01-21 21:33:22 +0530

    Update base.js

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDZq9KhVeJeip0zH6A18dWC7tkpm36aWqc",
-    authDomain: "catch-of-the-day-nikhil-namal.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-nikhil-namal.firebaseio.com",
+    apiKey: "your api key",
+    authDomain: "your app domain",
+    databaseURL: "your database url",
    
 });
 
