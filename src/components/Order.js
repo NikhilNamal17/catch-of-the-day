a0c8456f79069d243d653d60470058e8ee7e8a2d
NikhilNamal17 <nikhilnamal16@gmail.com>
2019-01-23 18:34:14 +0530

    update push

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,70 +1,68 @@
-import React from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import propTypes from 'prop-types';
-import {formatPrice} from '../helpers';
+import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import propTypes from "prop-types";
+import { formatPrice } from "../helpers";
 
- class Order extends React.Component{
-
+class Order extends React.Component {
     static propTypes = {
         fishes: propTypes.object,
         order: propTypes.object,
-        removeOrder: propTypes.func.isRequired,
-
-    }
+        removeOrder: propTypes.func
+    };
 
     renderOrder = key => {
-         const fish = this.props.fishes[key];
-         const count = this.props.order[key];
-         const isAvailable = fish && fish.status === "available"; 
-         const transitionOptions = {
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+        const isAvailable = fish && fish.status === "available";
+        const transitionOptions = {
             classNames: "order",
             key,
             timeout: { enter: 500, exit: 500 }
         };
-         if (!fish) return null;
-         if(!isAvailable){
-             return (
-                 <CSSTransition {...transitionOptions}>
+        if (!fish) return null;
+        if (!isAvailable) {
+            return (
+                <CSSTransition {...transitionOptions}>
                     <li key={key}>
-                        Sorry {fish? fish.name : "Fish"} is no longer available
+                        Sorry {fish ? fish.name : "Fish"} is no longer available
                     </li>
-                 </CSSTransition>
-             );
-         } 
-         return (
+                </CSSTransition>
+            );
+        }
+        return (
             <CSSTransition {...transitionOptions}>
-
                 <li key={key}>
-                <span>
-                    <TransitionGroup component="span" className="count">
-                        <CSSTransition
-                        classNames="count"
-                        key={count}
-                        timeout={{ enter: 500, exit: 500 }}>
-                        <span>{count}</span>
-                        </CSSTransition>
-                    </TransitionGroup>
-                    kgs {fish.name}
-                    {formatPrice(count * fish.price)}
-                    <button onClick={() => this.props.removeOrder(key)}>
-                    &times;
-                    </button>
-                </span>
+                    <span>
+                        <TransitionGroup component="span" className="count">
+                            <CSSTransition
+                                classNames="count"
+                                key={count}
+                                timeout={{ enter: 500, exit: 500 }}
+                            >
+                                <span>{count}</span>
+                            </CSSTransition>
+                        </TransitionGroup>
+                        kgs {fish.name}
+                        {formatPrice(count * fish.price)}
+                        <button onClick={() => this.props.removeOrder(key)}>
+                            &times;
+                        </button>
+                    </span>
                 </li>
             </CSSTransition>
-         );
+        );
     };
 
-    render(){
+    render() {
         const orderIds = Object.keys(this.props.order);
         const total = orderIds.reduce((prevTotal, key) => {
             const fish = this.props.fishes[key];
             const count = this.props.order[key];
             const isAvailable = fish && fish.status === "available";
-                if (isAvailable) {
-                    return prevTotal + count * fish.price;
-                }
-                return prevTotal;
+            if (isAvailable) {
+                return prevTotal + count * fish.price;
+            }
+            return prevTotal;
         }, 0);
         return (
             <div className="order-wrap">
--- src/components/Storepicker.js
+++ src/components/Storepicker.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import propTypes from 'prop-types';
-import { getFunName } from '../helpers';
+import React from "react";
+import propTypes from "prop-types";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-
     static propTypes = {
         history: propTypes.object
-    }
+    };
 
     myInput = React.createRef();
 
@@ -16,19 +15,22 @@ class StorePicker extends React.Component {
 
         //get the input
         var storeName = this.myInput.current.value;
-    
+
         //go to route by user
         this.props.history.push(`/store/${storeName}`);
     };
 
-
-    render() { 
+    render() {
         return (
             <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" ref={this.myInput} 
-                required placeholder="Store Name" 
-                defaultValue={getFunName()} />
+                <input
+                    type="text"
+                    ref={this.myInput}
+                    required
+                    placeholder="Store Name"
+                    defaultValue={getFunName()}
+                />
                 <button type="submit">Vist Store</button>
             </form>
         );
