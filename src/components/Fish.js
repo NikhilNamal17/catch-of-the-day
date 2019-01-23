07b3bb6b800760f17967d28570e9f9c5564c2033
NikhilNamal17 <nikhilnamal16@gmail.com>
2019-01-23 18:29:03 +0530

    update push

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import propTypes from 'prop-types';
-import { formatPrice } from '../helpers';
+import React from "react";
+import propTypes from "prop-types";
+import { formatPrice } from "../helpers";
 
-class Fish extends React.Component{
-
+class Fish extends React.Component {
     static propTypes = {
         details: propTypes.shape({
             image: propTypes.string,
             name: propTypes.string,
             desc: propTypes.string,
             status: propTypes.string,
-            price: propTypes.number,
+            price: propTypes.number
         }),
-        addToOrder: propTypes.func.isRequired,
-    }
+        addToOrder: propTypes.func
+    };
 
     render() {
-        const {image, name, price, desc, status} = this.props.details;
+        const { image, name, price, desc, status } = this.props.details;
         const isAvailable = status === "available";
-        
 
         return (
-          <li className="menu-fish">
-                <img src={image} alt={name}/>
+            <li className="menu-fish">
+                <img src={image} alt={name} />
                 <h3 className="fish-name">
                     {name}
                     <span className="price">{formatPrice(price)}</span>
                 </h3>
                 <p>{desc}</p>
-                <button disabled={!isAvailable} 
-                    onClick={() => {this.props.addToOrder(this.props.index)}}>
-                    {isAvailable? "Add To Cart" : "Sold Out!"}
+                <button
+                    disabled={!isAvailable}
+                    onClick={() => {
+                        this.props.addToOrder(this.props.index);
+                    }}
+                >
+                    {isAvailable ? "Add To Cart" : "Sold Out!"}
                 </button>
-
-          </li>
-
+            </li>
         );
     }
 }
